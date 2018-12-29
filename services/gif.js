afd9c044918145c289fa8fd975b1d4d57fc36b16
Pamela Abreu <pamelaabreu@pursuit.com>
2018-12-29 16:30:04 -0500

    updated searchGif with callback parameter

--- services/gif.js
+++ services/gif.js
@@ -2,21 +2,23 @@ const request = require('request');
 
 const gif = {};
 
-gif.searchGif = (search) => {
+gif.searchGif = (search, cb) => {
 
-    const api_key = `02QCU6RW3UFQoJvWti2qN42ya222mCcX`;
-    const limit = 2;
-    const url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}&limit=${limit}`;
-    
-    request(url, function (error, response, body) {
-        const data = JSON.parse(body);
-        const urls = []
-        for (const image of data.data) {
-            let imgUrl = image.images.original.url;
-            urls.push(imgUrl);
-        }
-        return urls;
-    });
+        const api_key = `02QCU6RW3UFQoJvWti2qN42ya222mCcX`;
+        const limit = 25;
+        const url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}&limit=${limit}`;
+        
+        
+        request(url, function (error, response, body) {
+            const data = JSON.parse(body);
+            let urls = []
+            for (const image of data.data) {
+                let imgUrl = image.images.original.url;
+                urls.push(imgUrl);
+            }
+            // return urls;
+            cb(urls)
+        });
 
 }
 
